fb7885bd1d45ee7ca5fdc3444d2d3b6cf7283241
shadab <me.shadab19@gmail.com>
2022-10-07 00:43:17 +0530

    [Added- initialize deck of cards]

---
+++ DeckOfCards/players.java
@@ -0,0 +1,24 @@
+package com.DeckOfCards;
+import java.util.*;
+public class players {
+
+	public static void main(String[] args) {
+		//calling methods
+		System.out.println("\nPress 1 ->> To view cards of 4 Players");
+		DeckOfCards cards = new DeckOfCards();
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+
+		switch (num) {
+		case 1:
+			cards.getCardCombination();
+			cards.getPlayer();
+			break;
+		default:
+			System.out.println("!!invalid input!!");
+			break;
+		}
+
+	}
+
+}
